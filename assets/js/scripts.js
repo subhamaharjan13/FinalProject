f16e3dff3381abd25b9f4e5afa1b663be1a0fd97
Suvechhaya <shuvechha31@gmail.com>
2018-11-15 12:52:32 +0545

    spineLine committed.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,18 @@
 (function($) {
     "use strict";
 
-    /* 
+    /*
     ------------------------------------------------
     Sidebar open close animated humberger icon
     ------------------------------------------------*/
 
     $(".hamburger").on('click', function() {
-        $(this).toggleClass("is-active");
+      console.log('m clicked........', $(this));
+        $(this).addClass("is-active");
     });
 
 
-    /*  
+    /*
     -------------------
     List item active
     -------------------*/
@@ -31,7 +32,7 @@
 
 
 
-    /*  
+    /*
     -----------------
     Chat Sidebar
     ---------------------*/
@@ -73,7 +74,7 @@
     });
 
 
-    /* TO DO LIST 
+    /* TO DO LIST
     --------------------*/
     $(".tdl-new").on('keypress', function(e) {
         var code = (e.keyCode ? e.keyCode : e.which);
@@ -158,21 +159,21 @@
                 $('#search').addClass('open');
                 $('#search > form > input[type="search"]').focus();
             });
-            
+
             $('#search, #search button.close').on('click keyup', function(event) {
                 if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
                     $(this).removeClass('open');
                 }
             });
-            
-            
+
+
             //Do not include! This prevents the form from submitting for DEMO purposes only!
             $('form').submit(function(event) {
                 event.preventDefault();
                 return false;
             })
 
-    
+
 
     /*  pace Loader
     -------------*/
